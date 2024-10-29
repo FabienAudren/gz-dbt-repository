7d579bd95643fe6d58bc635560b8d8ec79b222ae
Fabien Audren <fabienaudren0@gmail.com>
2024-10-29 15:21:14 +0000

    creating int_orders_margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,5 +1,9 @@
 select
-    date_date
-    ,round((SUM(quantity) * SUM(purchase_price)), 2) AS purchase_cost
-    ,round((SUM(revenue) - (SUM(quantity) * SUM(purchase_price))), 2) AS margin
+    orders_id
+    ,date_date
+    ,round(SUM(revenue), 2) AS total_revenue
+    ,round(SUM(quantity), 2) AS total_quantity
+    ,round(SUM(purchase_cost), 2) AS purchase_cost
+    ,round(SUM(margin), 2) AS margin
 from {{ ref('int_sales_margin') }} 
+group by orders_id, date_date
